e43cc287d1de20625dbfd1c600061cbd80c58472
nguyenlong03 <nguyenlong682003@gmail.com>
2024-11-27 22:05:52 +0700

    class

---
+++ Thuctap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipsManageApp
+{
+    internal class Thuctap
+    {
+        public class thuctap
+        {
+            public string name { get; set; }
+            public DateTime thoigianbatdau { get; set; }
+            public DateTime thoigianketthuc { get; set; }
+        }
+    }
+}
